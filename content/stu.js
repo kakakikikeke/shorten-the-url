fb93b370115e934349c44eeed9ec9bfaf3e092e7
cdl.yoshinaka@gmail.com <cdl.yoshinaka@gmail.com>
2012-06-17 19:02:08 +0900

    first commit

---
+++ content/stu.js
@@ -0,0 +1,363 @@
+var url = "";
+
+function shortenURL() {
+	var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+			.getService(Components.interfaces.nsIWindowMediator);
+	var mainWindow = wm.getMostRecentWindow("navigator:browser");
+	var tabBrowser = mainWindow.getBrowser();
+	var selectedText = tabBrowser.contentWindow.getSelection();
+	url = selectedText.toString().trim();
+	if (url.toString().startsWith("http") || url.toString().startsWith("https")) {
+		doBitlyAPI();
+	} else {
+		url = getURLOnHref().toString().trim();
+		if (url.toString().startsWith("http")
+				|| url.toString().startsWith("https")) {
+			doBitlyAPI();
+		} else {
+			window.alert("Not the correct URL");
+		}
+	}
+};
+
+function doBitlyAPI() {
+	var login = 'kakakikikeke4bitlyapi';
+	var apiKey = 'R_344ad8cbf73d813b1b28fa75a73ca60a';
+	bitly = 'http://api.bit.ly/shorten' + '?version=2.0.1&format=json&login='
+			+ login + '&apiKey=' + apiKey + '&longUrl=' + url;
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = bitly;
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = showResult;
+	xmlhttp.open('GET', script.src, true);
+	xmlhttp.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
+	xmlhttp.send();
+};
+
+function showResult() {
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		var response = xmlhttp.responseText;
+		var responseAsJSON = JSON.stringify(response);
+		var data = JSON.parse(response);
+		var result = data.results[url].shortUrl;
+		var gClipboardHelper = Components.classes["@mozilla.org/widget/clipboardhelper;1"]
+				.getService(Components.interfaces.nsIClipboardHelper);
+		gClipboardHelper.copyString(result);
+		window.prompt("Success", result);
+	}
+};
+
+function getURLOnHref() {
+	var urlOnHref = "";
+	var tag = document.popupNode.tagName;
+	if (tag.toString() == "A") {
+//		urlOnHref = document.popupNode.getAttribute("href");
+		urlOnHref = document.popupNode.href;
+	} else {
+		urlOnHref = document.popupNode.innerHTML;
+	}
+	return urlOnHref;
+};
+
+String.trim = function() {
+	return this
+			.replace(
+					/^[\s　]+|[\s　]+$|^[\s ]+|[\s ]+$|^[\n]+|[\n]+$|^[\r\n]+|[\r\n]+$|<.+?>/g,
+					'');
+};
+
+String.prototype.startsWith = function(str) {
+	return this.indexOf(str) == 0;
+};
+
+var json_parse = function() {
+
+	// This is a function that can parse a JSON text, producing a JavaScript
+	// data structure. It is a simple, recursive descent parser.
+
+	// We are defining the function inside of another function to avoid creating
+	// global variables.
+
+	var at, // The index of the current character
+	ch, // The current character
+	escapee = {
+		'"' : '"',
+		'\\' : '\\',
+		'/' : '/',
+		b : 'b',
+		f : '\f',
+		n : '\n',
+		r : '\r',
+		t : '\t'
+	}, text,
+
+	error = function(m) {
+
+		// Call error when something is wrong.
+
+		throw {
+			name : 'SyntaxError',
+			message : m,
+			at : at,
+			text : text
+		};
+	},
+
+	next = function(c) {
+
+		// If a c parameter is provided, verify that it matches the current
+		// character.
+
+		if (c && c !== ch) {
+			error("Expected '" + c + "' instead of '" + ch + "'");
+		}
+
+		// Get the next character. When there are no more characters,
+		// return the empty string.
+
+		ch = text.charAt(at);
+		at += 1;
+		return ch;
+	},
+
+	number = function() {
+
+		// Parse a number value.
+
+		var number, string = '';
+
+		if (ch === '-') {
+			string = '-';
+			next('-');
+		}
+		while (ch >= '0' && ch <= '9') {
+			string += ch;
+			next();
+		}
+		if (ch === '.') {
+			string += '.';
+			while (next() && ch >= '0' && ch <= '9') {
+				string += ch;
+			}
+		}
+		if (ch === 'e' || ch === 'E') {
+			string += ch;
+			next();
+			if (ch === '-' || ch === '+') {
+				string += ch;
+				next();
+			}
+			while (ch >= '0' && ch <= '9') {
+				string += ch;
+				next();
+			}
+		}
+		number = +string;
+		if (isNaN(number)) {
+			error("Bad number");
+		} else {
+			return number;
+		}
+	},
+
+	string = function() {
+
+		// Parse a string value.
+
+		var hex, i, string = '', uffff;
+
+		// When parsing for string values, we must look for " and \ characters.
+
+		if (ch === '"') {
+			while (next()) {
+				if (ch === '"') {
+					next();
+					return string;
+				} else if (ch === '\\') {
+					next();
+					if (ch === 'u') {
+						uffff = 0;
+						for (i = 0; i < 4; i += 1) {
+							hex = parseInt(next(), 16);
+							if (!isFinite(hex)) {
+								break;
+							}
+							uffff = uffff * 16 + hex;
+						}
+						string += String.fromCharCode(uffff);
+					} else if (typeof escapee[ch] === 'string') {
+						string += escapee[ch];
+					} else {
+						break;
+					}
+				} else {
+					string += ch;
+				}
+			}
+		}
+		error("Bad string");
+	},
+
+	white = function() {
+
+		// Skip whitespace.
+
+		while (ch && ch <= ' ') {
+			next();
+		}
+	},
+
+	word = function() {
+
+		// true, false, or null.
+
+		switch (ch) {
+			case 't' :
+				next('t');
+				next('r');
+				next('u');
+				next('e');
+				return true;
+			case 'f' :
+				next('f');
+				next('a');
+				next('l');
+				next('s');
+				next('e');
+				return false;
+			case 'n' :
+				next('n');
+				next('u');
+				next('l');
+				next('l');
+				return null;
+		}
+		error("Unexpected '" + ch + "'");
+	},
+
+	value, // Place holder for the value function.
+
+	array = function() {
+
+		// Parse an array value.
+
+		var array = [];
+
+		if (ch === '[') {
+			next('[');
+			white();
+			if (ch === ']') {
+				next(']');
+				return array; // empty array
+			}
+			while (ch) {
+				array.push(value());
+				white();
+				if (ch === ']') {
+					next(']');
+					return array;
+				}
+				next(',');
+				white();
+			}
+		}
+		error("Bad array");
+	},
+
+	object = function() {
+
+		// Parse an object value.
+
+		var key, object = {};
+
+		if (ch === '{') {
+			next('{');
+			white();
+			if (ch === '}') {
+				next('}');
+				return object; // empty object
+			}
+			while (ch) {
+				key = string();
+				white();
+				next(':');
+				object[key] = value();
+				white();
+				if (ch === '}') {
+					next('}');
+					return object;
+				}
+				next(',');
+				white();
+			}
+		}
+		error("Bad object");
+	};
+
+	value = function() {
+
+		// Parse a JSON value. It could be an object, an array, a string, a
+		// number,
+		// or a word.
+
+		white();
+		switch (ch) {
+			case '{' :
+				return object();
+			case '[' :
+				return array();
+			case '"' :
+				return string();
+			case '-' :
+				return number();
+			default :
+				return ch >= '0' && ch <= '9' ? number() : word();
+		}
+	};
+
+	// Return the json_parse function. It will have access to all of the above
+	// functions and variables.
+
+	return function(source, reviver) {
+		var result;
+
+		text = source;
+		at = 0;
+		ch = ' ';
+		result = value();
+		white();
+		if (ch) {
+			error("Syntax error");
+		}
+
+		// If there is a reviver function, we recursively walk the new
+		// structure,
+		// passing each name/value pair to the reviver function for possible
+		// transformation, starting with a temporary boot object that holds the
+		// result
+		// in an empty key. If there is not a reviver function, we simply return
+		// the
+		// result.
+
+		return typeof reviver === 'function' ? function walk(holder, key) {
+			var k, v, value = holder[key];
+			if (value && typeof value === 'object') {
+				for (k in value) {
+					if (Object.hasOwnProperty.call(value, k)) {
+						v = walk(value, k);
+						if (v !== undefined) {
+							value[k] = v;
+						} else {
+							delete value[k];
+						}
+					}
+				}
+			}
+			return reviver.call(holder, key, value);
+		}({
+					'' : result
+				}, '') : result;
+
+	};
+}();
